b7b11e85d2aad2e1ce474fdc06b75da074ab9f24
AV tuitions <vinayd180@gmail.com>
2026-07-25 02:46:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,5 @@
-const CACHE_NAME = 'av-pro-v1';
-const urlsToCache = ['/'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
+const CACHE_NAME = 'av-pro-rt2025-v1';
+const urlsToCache = ['./','./index.html','./manifest.json','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(k => Promise.all(k.map(key => key!== CACHE_NAME && caches.delete(key))))));
